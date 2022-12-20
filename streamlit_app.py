3866438071cf3a9cab90070c13ad948dc6640f71
Carol J Buenaventura <121069547+test-cjb8@users.noreply.github.com>
2022-12-20 11:44:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmel')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
